555cdeb37cbd14d854f1eff2a499440b19192f95
20923 <2092368489@qq.com>
2025-07-28 15:24:03 +0800

    实现usage

--- src/main/java/cn/tyt/llmproxy/dto/request/StatisticsQueryDto.java
+++ src/main/java/cn/tyt/llmproxy/dto/request/StatisticsQueryDto.java
@@ -8,9 +8,9 @@ import java.time.LocalDate;
 @Data
 public class StatisticsQueryDto {
 
-    //要查询的模型ID（数据库主键），可选
+    //要查询的模型ID（数据库主键），可选，与modelIdentifier二选一，优先此字段
     private Integer modelId;
-
+    //要查询的模型名，可选
     private String modelIdentifier;
 
     //查询日期 (格式: yyyy-MM-dd)，可选。
